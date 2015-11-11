049956cc3a7d32e0da5a874d1847d936c3037f25
alialani <alialani0719@gmail.com>
2015-11-10 20:47:48 -0800

    Configured Youtube API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,13 @@
  * Controller of the angularAppApp
  */
 angular.module('angularAppApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, current, $sce) {
+
+    $scope.nbaVideoResults = current.query();
+
+    $scope.refreshCurrent = function() {
+    	var myHtml = '<iframe ng-if="currentVid" width="560" height="315" src="https://www.youtube.com/embed/' + $scope.nbaVideoResults.items[0].id.videoId + '" frameborder="0" allowfullscreen></iframe>';
+    	$scope.currentVid = $sce.trustAsHtml(myHtml);
+    };
+
   });
